a2fcbffe9e36ae3b6cbf2e8b069726191562ba4f
rahullm9 <rahulmahato10101@gmail.com>
2026-02-23 04:43:10 +0530

    database checking done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -10,23 +10,36 @@ function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await API.post("/auth/register", form);
-    alert("Registered Successfully");
+    try {
+      await API.post("/auth/register", form);
+      alert("Registered Successfully");
+      setForm({
+        name: "",
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+      alert("Registration Failed");
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         placeholder="Name"
+        value={form.name}
         onChange={(e) => setForm({ ...form, name: e.target.value })}
       />
       <input
         placeholder="Email"
+        value={form.email}
         onChange={(e) => setForm({ ...form, email: e.target.value })}
       />
       <input
         type="password"
         placeholder="Password"
+        value={form.password}
         onChange={(e) => setForm({ ...form, password: e.target.value })}
       />
       <button>Register</button>
