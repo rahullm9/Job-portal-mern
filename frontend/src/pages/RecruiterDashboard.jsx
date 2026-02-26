fe54fb83ef33f12b155e1aad3fb36d4047baf9e3
rahullm9 <rahulmahato10101@gmail.com>
2026-02-27 02:36:31 +0530

    type of user based dashboard sidebar layout setup done

--- frontend/src/pages/RecruiterDashboard.jsx
+++ frontend/src/pages/RecruiterDashboard.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import DashboardSidebar from "../components/DashboardSidebar";
 
 const RecruiterDashboard = () => {
-  return <div>welcome recruiter</div>;
+  return (
+    <div className="dashboard-layout">
+      <DashboardSidebar role="recruiter" />
+      <main className="dashboard-main">
+        <h1>Recruiter Dashboard</h1>
+        <p>Welcome recruiter</p>
+      </main>
+    </div>
+  );
 };
 
 export default RecruiterDashboard;
