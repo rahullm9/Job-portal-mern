19a44d909a0269cbffc5500a39c39763b28972fe
Rahul Mahato <rahulmahato10101@gmail.com>
2026-05-08 01:22:40 +0530

    vercel setup for deploying

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
+
+app.use(express.static(path.resolve(__dirname, "frontend", "build"))); 
+app.use((req, res) => { 
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); 
+});
 
 const PORT = process.env.PORT || 5000;
 
